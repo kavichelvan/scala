8862971d84d7242b6c4d1be3a4f8bdb1d7cc4c50
kavichelvan <41743539+kavichelvan@users.noreply.github.com>
2018-07-27 16:18:41 +0530

    Add files via upload

---
+++ first_object.scala
@@ -0,0 +1,14 @@
+
+
+object first_object{
+   def main(args: Array[String]): Unit = {
+    
+		 print ("Hello world")
+		 
+		 def min(x: Int,y: Int): Int  = if (x<y) x else y
+		 
+		 println("\nCalling method")
+		 println(min(30,20))
+  }
+
+}
